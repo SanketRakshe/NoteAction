ee7f836e3e945e58bb6137fd4657276944e27904
sanketrakshe <sanketrakshe07@gmail.com>
2024-03-01 15:31:06 +0530

    Feat: Changes in client convex provider

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -4,7 +4,7 @@ import { ConvexReactClient } from 'convex/react';
 import { ConvexProviderWithClerk } from 'convex/react-clerk';
 import { ClerkProvider, useAuth } from '@clerk/nextjs';
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
 
 export default function ConvexClientProvider({
   children,
